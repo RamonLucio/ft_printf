f594f82adee6b7fde65e1115268913bbda20b8a2
Ramon <ramonluciolinhares@gmail.com>
2021-10-11 16:31:32 -0300

    Testing c conversion

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: rlucio-l <rlucio-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 15:03:03 by rlucio-l          #+#    #+#             */
-/*   Updated: 2021/10/11 13:39:48 by rlucio-l         ###   ########.fr       */
+/*   Updated: 2021/10/11 16:29:37 by rlucio-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 // Don't forget to remove below
-#include <stdio.h>
+/* #include <stdio.h>
 
 void	ft_putchar_fd(char c, int fd)
 {
@@ -26,35 +26,38 @@ void	ft_putstr_fd(char *s, int fd)
 		ft_putchar_fd(*s, fd);
 		s++;
 	}
-}
+} */
 // Don't forget to remove above
 
 int	ft_printf(const char *format, ...)
 {
-	va_list		arg_ptr;
-	char		*sval;
-	int			ival;
-	double		dval;
+	va_list			arg_ptr;
+	int 			char_counter;
+	unsigned char	chval;
+	char			*sval;
+	int				ival;
 
 	va_start(arg_ptr, format);
+	char_counter = 0;
 	while (*format)
 	{
+		char_counter++;
 		if (*format != '%')
 			ft_putchar_fd(*format++, 1);
 		else
 		{
 			format++;
-			if (*format == 'd')
+			if (*format == 'c')
 			{
-				ival = va_arg(arg_ptr, int);
-				printf("%d", ival);
+				chval = va_arg(arg_ptr, int);
+				ft_putchar_fd(chval, 1);
 				format++;
 				continue ;
 			}
-			else if (*format == 'f')
+/* 			if (*format == 'd')
 			{
-				dval = va_arg(arg_ptr, double);
-				printf("%f", dval);
+				ival = va_arg(arg_ptr, int);
+				ft_putnbr_fd(ival, 1);
 				format++;
 				continue ;
 			}
@@ -64,7 +67,7 @@ int	ft_printf(const char *format, ...)
 				ft_putstr_fd(sval, 1);
 				format++;
 				continue ;
-			}
+			} */
 			else
 			{
 				ft_putchar_fd(*format, 1);
@@ -74,16 +77,19 @@ int	ft_printf(const char *format, ...)
 		}
 	}
 	va_end(arg_ptr);
-	return (0);
+	return (char_counter);
 }
 
-int	main(void)
+/* int main(void)
 {
-	char	*name;
-	char	*pilpil;
-
-	name = "Ramon";
-	pilpil = "Formiga pilpil";
-	ft_printf("Me chamo %s e moro com %s\n", name, pilpil);
+	ft_printf("%c\n", '0');
+	ft_printf(" %c \n", '0');
+	ft_printf(" %c\n", '0' - 256);
+	ft_printf("%c \n", '0' + 256);
+	ft_printf(" %c %c %c \n", '0', 0, '1');
+	ft_printf(" %c %c %c \n", ' ', ' ', ' ');
+	ft_printf(" %c %c %c \n", '1', '2', '3');
+	ft_printf(" %c %c %c \n", '2', '1', 0);
+	ft_printf(" %c %c %c \n", 0, '1', '2');
 	return (0);
-}
+} */
